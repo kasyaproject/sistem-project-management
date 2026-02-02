dcae5a0699e8421a67ccf37f384419bcad28f633
Andika Syamsiana <andikayamana14045@gmail.com>
2026-02-02 22:17:11 +0700

    add CardAssignee, CardAttachment, and CardLabel models with appropriate fields and UUID handling

--- models/card_position.model.go
+++ models/card_position.model.go
@@ -5,7 +5,7 @@ import (
 	"github.com/kasyaproject/sistem-project-management/models/types"
 )
 
-type Card_Position struct {
+type CardPosition struct {
 	InternalID int64     `json:"internal_id" gorm:"primaryKey;autoIncrement"`
 	PublicID   uuid.UUID `json:"public_id" gorm:"type:uuid;not null"`
 	ListID     int64     `json:"list_internal_id" gorm:"column:list_internal_id:not null"`
--- models/list_position.model.go
+++ models/list_position.model.go
@@ -5,7 +5,7 @@ import (
 	"github.com/kasyaproject/sistem-project-management/models/types"
 )
 
-type List_Position struct {
+type ListPosition struct {
 	InternalID int64     `json:"internal_id" db:"internal_id" gorm:"primaryKey;autoIncrement"`
 	PublicID   uuid.UUID `json:"public_id" db:"public_id"`
 	BoardID    int64     `json:"board_internal_id" db:"board_internal_id" gorm:"column: board_internal_id"`
