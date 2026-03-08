0b3e81dc471e787849038408ad397b42775c40a5
Andika Syamsiana <andikayamana14045@gmail.com>
2026-03-08 14:24:20 +0700

    add JWT token generation and response handling utilities

--- config/config.go
+++ config/config.go
@@ -12,45 +12,43 @@ import (
 )
 
 var (
-	DB *gorm.DB
+	DB        *gorm.DB
 	AppConfig *Config
 )
 
 // Type dari env
 type Config struct {
-	AppPort string
-	DBHost string
-	DBPort string
-	DBUser string
+	AppPort    string
+	DBHost     string
+	DBPort     string
+	DBUser     string
 	DBPassword string
-	DBName string
+	DBName     string
 
-	JWTSecret string
-	JWTExpireMinutes string
+	JWTSecret       string
 	JWTRefreshToken string
-	JWTExpire string
+	JWTExpire       string
 }
 
-// Function untuk memanggil file ENV  
-func LoadEnv(){
- 	err := godotenv.Load()
+// Function untuk memanggil file ENV
+func LoadEnv() {
+	err := godotenv.Load()
 
 	if err != nil {
 		log.Println("No .env file found!")
 	}
 
 	AppConfig = &Config{
-		AppPort: getEnv("PORT", "3000"),
-		DBHost: getEnv("DB_HOST", "localhost"),
-		DBPort: getEnv("DB_PORT", "5432"),
-		DBUser: getEnv("DB_USER", "postgres"),
+		AppPort:    getEnv("PORT", "3000"),
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
 		DBPassword: getEnv("DB_PASSWORD", "swordfish"),
-		DBName: getEnv("DB_NAME", "sistem_project_management"),
+		DBName:     getEnv("DB_NAME", "sistem_project_management"),
 
-		JWTSecret: getEnv("JWT_SECRET", ""),
-		JWTExpireMinutes: getEnv("JWT_EXPIRY_MINUTES","60"),
-		JWTRefreshToken: getEnv("REFRESH_TOKEN_EXPIRES_IN","24h"),
-		JWTExpire: getEnv("JWT_EXPIRES_IN","1h"),
+		JWTSecret:       getEnv("JWT_SECRET", ""),
+		JWTRefreshToken: getEnv("REFRESH_TOKEN_EXPIRES_IN", "24h"),
+		JWTExpire:       getEnv("JWT_EXPIRES_IN", "1h"),
 	}
 }
 
@@ -58,30 +56,30 @@ func LoadEnv(){
 func getEnv(key string, fallback string) string {
 	value, exist := os.LookupEnv(key)
 
-	if exist{
+	if exist {
 		// Jika ada return value env nya
 		return value
-	}else{
+	} else {
 		// Jika tidak return default
 		return fallback
 	}
 }
 
 // Function untuk connect ke DB
-func ConncetDB(){
-	 // Ambil konfigurasi database dari struct AppConfig
+func ConncetDB() {
+	// Ambil konfigurasi database dari struct AppConfig
 	cfg := AppConfig
 
-	// Buat string koneksi ke PostgresSQL 
-	dsn:= fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName )
+	// Buat string koneksi ke PostgresSQL
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
 
 	// Koneksi ke DB
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil{
+	if err != nil {
 		log.Fatal("Failed to connect to database", err)
 	}
 
-	 // Mengambil instance database
+	// Mengambil instance database
 	sqlDB, err := db.DB()
 	if err != nil {
 		log.Fatal("Failed to get database instance", err)
