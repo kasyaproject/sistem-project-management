b4ed9b8258f365dff59f058a46a41676849efa40
Andika Syamsiana <andikasyamsiana@wdh.ac.id>
2026-03-11 14:37:45 +0700

    fix: reorder GetUser route definition in user group

--- routes/route.go
+++ routes/route.go
@@ -32,6 +32,6 @@ func Setup(app *fiber.App, uc *controllers.UserController) {
 
 	// Route need jwt token
 	userGroup := api.Group("/user")
-	userGroup.Get("/:id", uc.GetUser) // "/api/v1/user/:id"
 	userGroup.Get("/page", uc.FindAllUser)
+	userGroup.Get("/:id", uc.GetUser) // "/api/v1/user/:id"
 }
